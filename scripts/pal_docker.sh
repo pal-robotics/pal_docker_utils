59e15b19aae5079a10decf56e8d38ad758f0c01e
Victor Lopez <victor.lopez@pal-robotics.com>
2017-12-05 17:39:59 +0100

    Initial commit

---
+++ scripts/pal_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variables required for logging as a user with the same id as the user running this script
+export LOCAL_USER_ID=`id -u $USER`
+export LOCAL_GROUP_ID=`id -g $USER`
+export LOCAL_GROUP_NAME=`id -gn $USER` 
+
+# Settings required for having nvidia GPU acceleration inside the docker
+export QT_X11_NO_MITSHM=1
+
+# Settings required for ssh accesSettings required for ssh accesss
+nvidia-docker run -it \
+--env LOCAL_USER_ID --env LOCAL_GROUP_ID --env LOCAL_GROUP_NAME \
+--env DISPLAY --env QT_X11_NO_MITSHM --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+--net host --privileged \
+-v "$HOME/exchange:/home/user/exchange" \
+--mount source=erbium-volume,target=/home/user/ \
+$*
