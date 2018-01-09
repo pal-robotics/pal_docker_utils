4f781a1da3aef8d70228cacdaae9c8b59974e967
Victor Lopez <victor.lopez@pal-robotics.com>
2018-01-09 16:48:13 +0100

    Restructure and document

--- scripts/pal_docker.sh
+++ scripts/pal_docker.sh
@@ -3,16 +3,22 @@
 # Variables required for logging as a user with the same id as the user running this script
 export LOCAL_USER_ID=`id -u $USER`
 export LOCAL_GROUP_ID=`id -g $USER`
-export LOCAL_GROUP_NAME=`id -gn $USER` 
+export LOCAL_GROUP_NAME=`id -gn $USER`
+DOCKER_USER_ARGS="--env LOCAL_USER_ID --env LOCAL_GROUP_ID --env LOCAL_GROUP_NAME"
+
+# Variables for forwarding ssh agent into docker container
+SSH_AUTH_ARGS=""
+if [ ! -z $SSH_AUTH_SOCK ]; then
+    DOCKER_SSH_AUTH_ARGS="-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+fi
 
 # Settings required for having nvidia GPU acceleration inside the docker
-export QT_X11_NO_MITSHM=1
+DOCKER_GPU_ARGS="--env DISPLAY --env QT_X11_NO_MITSHM=1 --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw"
 
-# Settings required for ssh accesSettings required for ssh accesss
 nvidia-docker run -it \
---env LOCAL_USER_ID --env LOCAL_GROUP_ID --env LOCAL_GROUP_NAME \
---env DISPLAY --env QT_X11_NO_MITSHM --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
--v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
+$DOCKER_USER_ARGS \
+$DOCKER_GPU_ARGS \
+$DOCKER_SSH_AUTH_ARGS \
 --net host --privileged \
 -v "$HOME/exchange:/home/user/exchange" \
 $*
