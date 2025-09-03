81a0d117535c6598ff5e7a1e066373db9da81fad
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-09-03 12:36:39 +0000

    Add NVIDIA_DRIVER_CAPABILITIES to GPU enabled hardware

--- scripts/pal_docker.sh
+++ scripts/pal_docker.sh
@@ -24,9 +24,9 @@ HAS_NVIDIA_DOCKER=$?
 if [ $HAS_NVIDIA_TOOLKIT -eq 0 ]; then
   docker_version=`docker version --format '{{.Client.Version}}' | cut -d. -f1`
   if [ $docker_version -ge 19 ]; then
-	  DOCKER_COMMAND="docker run --gpus all"
+	  DOCKER_COMMAND="docker run --gpus all --env NVIDIA_DRIVER_CAPABILITIES=all"
   else
-	  DOCKER_COMMAND="docker run --runtime=nvidia"
+	  DOCKER_COMMAND="docker run --runtime=nvidia --env NVIDIA_DRIVER_CAPABILITIES=all"
   fi
 elif [ $HAS_NVIDIA_DOCKER -eq 0 ]; then
   DOCKER_COMMAND="nvidia-docker run"
